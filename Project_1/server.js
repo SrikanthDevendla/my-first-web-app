9cbd4af058b95e12bf1b7ee7399e19f8dd53633f
SrikanthDevendla <srikanthdevendlaa@gmail.com>
2023-05-20 15:09:14 +0530

    Initial commit

---
+++ Project_1/server.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var morgan = require('morgan');
+var path = require('path');
+
+var app = express();
+app.use(morgan('combined'));
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+app.get('/ui/style.css', function(req, res) {
+    res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+});
+
+app.get('/ui/image.jpg', function(req, res) {
+    res.sendFile(path.join(__dirname, 'ui', 'image.jpg'));
+});
+
+var port = 8080; // Use 8080 for local development because you might already have apache running on 80
+app.listen(8080, function() {
+    console.log('IMAD course app listening on port : ', port);
+});
